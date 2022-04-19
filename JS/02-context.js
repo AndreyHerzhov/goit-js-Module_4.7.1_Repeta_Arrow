c0ec9f8ad48894d13acc2384bacc1ca478a7ebc5
AndreyHerzhov <andreyherzhov@gmail.com>
2022-04-19 10:20:20 +0300

    close

--- JS/02-context.js
+++ JS/02-context.js
@@ -100,22 +100,69 @@
 //         return a - b;
 //     })
 
-const numbers = [1, 2, 3, 4, 5, 6]
+// const numbers = [1, 2, 3, 4, 5, 6]
 
-const filteredNum = numbers.filter(num => num > 2)
-console.log(filteredNum)
+// const filteredNum = numbers.filter(num => num > 2)
+// console.log(filteredNum)
 
-const mltplByTwo = filteredNum.map(num => num * 2)
-console.log(mltplByTwo)
+// const mltplByTwo = filteredNum.map(num => num * 2)
+// console.log(mltplByTwo)
 
-const sorted = mltplByTwo.sort((a,b) => b - a)
-console.log(sorted)
+// const sorted = mltplByTwo.sort((a,b) => b - a)
+// console.log(sorted)
 
-const res = numbers 
-    .filter(num => num > 2)
-    .map(num => num * 3)
-    .sort((a,b) =>  a - b)
+// const res = numbers 
+//     .filter(num => num > 2)
+//     .map(num => num * 3)
+//     .sort((a,b) =>  a - b)
 
 
-console.log(res)
+// console.log(res)
 
+
+
+/*
+ *   Callback
+ */
+
+// const first = function(y){
+//     console.log(1);
+//     y();
+// }
+
+// const second = function(a,b){
+//     console.log(a * b)
+// }
+
+// first(function(){
+//     second(5,7);
+// })
+
+
+
+/*
+ *   Замыкание
+ */
+
+
+// function createCalcFunction(n) {
+//     return function(){
+//         console.log(1000 * n)
+//     }
+// }
+
+// createCalcFunction(42) // ничего не вернет
+
+// const add = createCalcFunction(42)
+// add() // 42000
+
+// function createIncrement(n) {
+//     return function(num){
+//         console.log(n + num)
+//     }
+// }
+
+// const addTen = createIncrement(10)
+// const addOne = createIncrement(1)
+// addTen(10) // 20
+// addOne(10) // 11
